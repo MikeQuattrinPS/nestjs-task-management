074a2ee2bd87407fc5e207a66ed40e7dc759f419
Mike Quattrin <mike.quattrin@publicissapient.com>
2026-04-21 11:21:35 -0400

    Added PG using TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-	imports: [TasksModule],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: '.env',
+		}),
+		TasksModule,
+		TypeOrmModule.forRoot({
+			type: process.env.DB_TYPE as any,
+			host: process.env.DB_HOST,
+			port: parseInt(process.env.DB_PORT, 10),
+			username: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_DATABASE,
+			autoLoadEntities: true,
+			synchronize: true,
+		}),
+	],
 })
 export class AppModule {}
